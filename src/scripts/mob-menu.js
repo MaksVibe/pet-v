9786162006c57790bc159f6221c200d93f9cdfd4
MaksVibe <parunovmaksym@gmail.com>
2022-01-08 01:36:09 +0200

    Add button and closing mobile menu logic
    Add close-icon.
    Styles for the button and close-icon.
    Add function for closing mobile menu.

--- src/scripts/mob-menu.js
+++ src/scripts/mob-menu.js
@@ -1,8 +1,12 @@
 import refs from './refs';
 
 let isOpenMenu = false;
-const markup = `<div class="backdrop"></div>
+
+const markup = `
+<div class="backdrop"></div>
 <div class="mobile-menu">
+    <button class="close_btn" type="button" id="close_btn">
+    </button>
   <span class="mobile-menu__logo">Aurora</span>
   <ul class="mobile-menu__list">
     <li class="mobile-menu__item"><a href="/">what's new</a></li>
@@ -19,6 +23,7 @@ const markup = `<div class="backdrop"></div>
 const onClkickShowModal = e => {
   e.preventDefault();
   if (isOpenMenu) return false;
+  refs.navMenu.classList.toggle('visually-hidden');
 
   renderMobMenu();
 };
@@ -28,4 +33,17 @@ const renderMobMenu = () => {
   refs.navMenu.innerHTML = markup;
 };
 
+const closeMobileMenu = e => {
+  e.preventDefault();
+  if (!isOpenMenu) return false;
+  if (e.target.id.includes('close_btn')) {
+    refs.navMenu.classList.toggle('visually-hidden');
+    isOpenMenu = false;
+    refs.navMenu.innerHTML = '';
+  }
+};
+
+// Open modal
 refs.burgerMenu.addEventListener('click', onClkickShowModal);
+// Close modal
+refs.navMenu.addEventListener('click', closeMobileMenu);
