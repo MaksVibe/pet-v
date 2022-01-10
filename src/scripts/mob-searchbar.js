115d9aa737f24b88f6a328bbd2d8e2e44a7b3adb
MaksVibe <parunovmaksym@gmail.com>
2022-01-10 13:11:38 +0200

    End mobile-modal
    logic for closing mob-modal window, for click not on header

--- src/scripts/mob-searchbar.js
+++ src/scripts/mob-searchbar.js
@@ -1,7 +1,19 @@
 import refs from './refs';
 
 refs.searchBtn.addEventListener('click', toggleModal);
+document.addEventListener('click', handler);
+refs.closeSearchBarBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
   refs.searchBar.classList.toggle('visually-hidden');
+  refs.searchBtn.classList.toggle('visually-hidden');
+  refs.closeSearchBarBtn.classList.toggle('visually-hidden');
+}
+
+function handler(e) {
+  if (!e.target.closest('header') && e.target !== refs.closeSearchBarBtn) {
+    refs.searchBar.classList.add('visually-hidden');
+    refs.searchBtn.classList.remove('visually-hidden');
+    refs.closeSearchBarBtn.classList.add('visually-hidden');
+  }
 }
--- src/scripts/refs.js
+++ src/scripts/refs.js
@@ -1,6 +1,9 @@
 export default {
+  header: document.querySelector('.header'),
   searchBtn: document.getElementById('searchBtn'),
   searchBar: document.getElementById('searchbar'),
+  searchBarBtn: document.getElementById('mob-btn-iconsearch'),
+  closeSearchBarBtn: document.querySelector('.close_search'),
   navMenu: document.getElementById('mob-menu'),
   subMenu: document.getElementById('sub-menu'),
   burgerMenu: document.querySelector('.icon-menu-burger'),
